1a006d3e3f68679e06c509710622f64263f20158
mxiaozz <mxiaozz@qq.com>
2024-08-20 16:23:08 +0800

    增加 404 跳转首页处理

--- server/server.go
+++ server/server.go
@@ -4,20 +4,45 @@ import (
 	"github.com/gin-gonic/gin"
 	"vpn-web.funcworks.net/gb"
 	"vpn-web.funcworks.net/router"
+	"vpn-web.funcworks.net/util"
 )
 
 func LaunchWeb() {
-	gin.SetMode(gin.ReleaseMode)
+	// 设置gin模式
+	gin.SetMode(util.If(gb.Config.Server.DevMode, gin.DebugMode, gin.ReleaseMode))
+
+	// web engine
 	engine := gin.New()
 	engine.Use(gin.LoggerWithFormatter(ginLogFormat), gin.Recovery())
+
+	// 注册路由
 	router.Init(engine)
 
+	// 404
+	noRouteHandle(engine)
+
+	// 启动服务
+	engine.Run(getAddr())
+}
+
+func noRouteHandle(engine *gin.Engine) {
+	// 前后端分离，F5刷新时路径为前端路由器，后台将404，将调转至首页
+	engine.NoRoute(func(ctx *gin.Context) {
+		if ctx.Request.Method == "GET" {
+			ctx.Redirect(302, "/")
+		} else {
+			ctx.JSON(404, gin.H{
+				"code":    404,
+				"message": "Page Not Found",
+			})
+		}
+	})
+}
+
+func getAddr() string {
 	host := gb.Config.Server.Host
 	if host == "" && gb.Config.Server.DevMode {
-		gin.SetMode(gin.DebugMode)
 		host = "127.0.0.1"
 	}
-	port := gb.Config.Server.Port
-	addr := host + ":" + port
-	engine.Run(addr)
+	return host + ":" + gb.Config.Server.Port
 }
